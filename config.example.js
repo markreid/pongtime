3256524efa87a4e91db92db0485d6bda04fb31fb
Mark Reid <contact@markreid.com.au>
2014-04-21 12:22:12 +0800

    +config.example.js, gitignore config.js

---
+++ config.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    EXPRESS_PORT: 2020,
+    COOKIE_SECRET: 'put your cookie secret here',
+    DB: {
+        NAME: 'pongdb',
+        USER: 'dbuser',
+        PASS: 'dbpass',
+        DIALECT: 'postgres',
+        PORT: '5432',
+        HOST: 'localhost'
+    }
+}
