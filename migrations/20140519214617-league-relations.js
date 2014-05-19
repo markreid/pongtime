140f4e2d628f347027e4f5a29d0fe3ac49fdf6ae
Mark Reid <contact@markreid.com.au>
2014-05-19 21:52:31 +1000

    league relations migration

---
+++ migrations/20140519214617-league-relations.js
@@ -0,0 +1,42 @@
+/**
+ * Add all the relational columns for Leagues
+ */
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+
+    migration.addColumn('Games', 'leagueId', {
+        type: DataTypes.INTEGER,
+        references: 'Leagues',
+        referencesKey: 'leagueId',
+        allowNull: false
+    }).success(function(){
+        migration.addColumn('Players', 'leagueId', {
+            type: DataTypes.INTEGER,
+            references: 'Leagues',
+            referencesKey: 'leagueId',
+            allowNull: false
+        }).success(function(){
+            migration.addColumn('Teams', 'leagueId', {
+                type: DataTypes.INTEGER,
+                references: 'Leagues',
+                referencesKey: 'leagueId',
+                allowNull: false
+            }).success(function(){
+                done();
+            });
+        });
+    });
+  },
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    migration.removeColumn('Games', 'leagueId').success(function(){
+        migration.removeColumn('Players', 'leagueId').success(function(){
+            migration.removeColumn('Teams', 'leagueId').success(function(){
+                done();
+            });
+        });
+    });
+  }
+}
