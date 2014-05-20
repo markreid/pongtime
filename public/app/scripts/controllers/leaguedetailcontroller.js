13496e78476457ec00981120bb8536cea1c01b3b
Mark Reid <contact@markreid.com.au>
2014-05-21 09:13:51 +1000

    quick league stats

--- public/app/scripts/controllers/leaguedetailcontroller.js
+++ public/app/scripts/controllers/leaguedetailcontroller.js
@@ -47,34 +47,77 @@
             // player with most wins
             // player with most losses
 
-            var first = _.extend({}, _.find(league.teams, function(team){
-                return team.stat.games
+            var firstTeam = _.extend({}, _.find(league.teams, function(team){
+                return team.stat.games;
             }));
-            first.stat = statsService.parseStats(first.stat);
+            firstTeam.stat = statsService.parseStats(firstTeam.stat);
 
-            var teamMostWins = [first];
-            var teamMostLosses = [first];
-            var teamBestPercentage = [first];
-            var teamWorstPercentage = [first];
+            var teamMostWins = [firstTeam];
+            var teamMostLosses = [firstTeam];
+            var teamBestPercentage = [firstTeam];
+            var teamWorstPercentage = [firstTeam];
+            var teamHottestStreak = [firstTeam];
+            var teamColdestStreak = [firstTeam];
 
             _.each(league.teams, function(team){
                 if(!team.stat.games) return;
                 team.stat = statsService.parseStats(team.stat);
 
-                setHighest(teamMostWins, team, 'wins');
-                setHighest(teamMostLosses, team, 'losses');
-                setHighest(teamBestPercentage, team, 'winPercentage');
-                setHighest(teamWorstPercentage, team, 'lossPercentage');
+                teamMostWins = setHighest(teamMostWins, team, 'wins');
+                teamMostLosses = setHighest(teamMostLosses, team, 'losses');
+                teamBestPercentage = setHighest(teamBestPercentage, team, 'winPercentage');
+                teamWorstPercentage = setHighest(teamWorstPercentage, team, 'lossPercentage');
+                teamHottestStreak = setHighest(teamHottestStreak, team, 'hottest');
+                teamColdestStreak = setHighest(teamColdestStreak, team, 'coldest');
 
             });
 
-            var stats = [];
-            stats.push(humanizeStat('Most wins', teamMostWins, 'wins'));
-            stats.push(humanizeStat('Most losses', teamMostLosses, 'losses'));
-            stats.push(humanizeStat('Best win-rate', teamBestPercentage, 'winPercentage'));
-            stats.push(humanizeStat('Worst loss-rate', teamWorstPercentage, 'lossPercentage'));
+            var teamStats = [];
+            teamStats.push(humanizeStat('Most wins', teamMostWins, 'wins'));
+            teamStats.push(humanizeStat('Most losses', teamMostLosses, 'losses'));
+            teamStats.push(humanizeStat('Best win-rate', teamBestPercentage, 'winPercentage', true));
+            teamStats.push(humanizeStat('Worst loss-rate', teamWorstPercentage, 'lossPercentage', true));
+            teamStats.push(humanizeStat('Hottest win streak', teamHottestStreak, 'hottest'));
+            teamStats.push(humanizeStat('Coldest lose streak', teamColdestStreak, 'coldest'));
 
-            return stats;
+
+
+            var firstPlayer = _.extend({}, _.find(league.players, function(player){
+                return player.stat.games;
+            }));
+            firstPlayer.stat = statsService.parseStats(firstPlayer.stat);
+
+            var playerMostWins = [firstPlayer];
+            var playerMostLosses = [firstPlayer];
+            var playerBestPercentage = [firstPlayer];
+            var playerWorstPercentage = [firstPlayer];
+            var playerHottestStreak = [firstPlayer];
+            var playerColdestStreak = [firstPlayer];
+
+            _.each(league.players, function(player){
+                if(!player.stat.games) return;
+                player.stat = statsService.parseStats(player.stat);
+
+                playerMostWins = setHighest(playerMostWins, player, 'wins');
+                playerMostLosses = setHighest(playerMostLosses, player, 'losses');
+                playerBestPercentage = setHighest(playerBestPercentage, player, 'winPercentage');
+                playerWorstPercentage = setHighest(playerWorstPercentage, player, 'lossPercentage');
+                playerHottestStreak = setHighest(playerHottestStreak, player, 'hottest');
+                playerColdestStreak = setHighest(playerColdestStreak, player, 'coldest');
+            });
+
+            var playerStats = [];
+            playerStats.push(humanizeStat('Most wins', playerMostWins, 'wins'));
+            playerStats.push(humanizeStat('Most losses', playerMostLosses, 'losses'));
+            playerStats.push(humanizeStat('Best win-rate', playerBestPercentage, 'winPercentage', true));
+            playerStats.push(humanizeStat('Worst loss-rate', playerWorstPercentage, 'lossPercentage', true));
+            playerStats.push(humanizeStat('Hottest win streak', playerHottestStreak, 'hottest'));
+            playerStats.push(humanizeStat('Coldest lose streak', playerColdestStreak, 'coldest'));
+
+            return {
+                team: teamStats,
+                player: playerStats
+            };
 
         }
 
@@ -87,18 +130,31 @@
         function setHighest(arr, team, prop){
             if(team.stat[prop] > arr[0].stat[prop]){
                 arr = [team]
-                return;
+                return arr;
             }
             if(team.stat[prop] === arr[0].stat[prop] && arr[0].id !== team.id){
                 arr.push(team);
-                return;
+                return arr;
             }
+            return arr;
         }
 
-        function humanizeStat(title, teams, prop){
+        function setLowest(arr, team, prop){
+            if(team.stat[prop] < arr[0].stat[prop]){
+                arr = [team]
+                return arr;
+            }
+            if(team.stat[prop] === arr[0].stat[prop] && arr[0].id !== team.id){
+                arr.push(team);
+                return arr;
+            }
+            return arr;
+        }
+
+        function humanizeStat(title, teams, prop, percentage){
             return {
                 title: title,
-                value: _.pluck(teams, 'name').join(', ') + ' (' + teams[0].stat[prop] + ')'
+                value: _.pluck(teams, 'name').join(', ') + ' (' + teams[0].stat[prop] + (percentage ? '%' : '') + ')'
             };
         }
 
